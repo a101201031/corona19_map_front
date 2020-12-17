a2a0c0334f52bc5c5d57a48cf93ba2f4f5031d82
a101201031 <0825tngus@naver.com>
2020-12-17 23:17:54 +0900

    feat: fix function param-> none param

--- src/javascript/getCoronaInfo.ts
+++ src/javascript/getCoronaInfo.ts
@@ -1,29 +1,18 @@
 import axios from 'axios';
+import { ENV } from 'constants/ENV';
+import { CountryItem } from 'models';
 
-interface GlobalCoronaInfoTypes {
-  NewConfirmed: number;
-  TotalConfirmed: number;
-  NewDeaths: number;
-  TotalDeaths: number;
-  NewRecovered: number;
-  TotalRecovered: number;
-  LastUpdate: Date;
-}
+const httpClient = axios.create({
+  baseURL: ENV.API_URL,
+});
 
-async function getCoronaInfo(url: {
-  baseUrl: string;
-  reqUrl: string;
-}): Promise<GlobalCoronaInfoTypes> {
-  return new Promise((resolve, rejects) => {
-    axios
-      .get(`${url.baseUrl}${url.reqUrl}`)
-      .then((res) => {
-        resolve(res.data.data[0]);
-      })
-      .catch((err) => {
-        rejects();
-      });
-  });
-}
-
-export default getCoronaInfo;
+export const getCoronaInfo = async () => {
+  try {
+    const { data } = await httpClient.get<{ data: CountryItem[] }>(
+      '/countries-corona-info',
+    );
+    return data.data;
+  } catch {
+    return [];
+  }
+};
