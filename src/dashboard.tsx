602e50ee20ecd2d10064198b82d9c1845a4e8c50
a101201031 <0825tngus@naver.com>
2020-12-19 00:13:23 +0900

    feat: add ranking, fix global -> countries

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,36 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import getCoronaInfo from 'javascript/getCoronaInfo';
-import InfoList from 'infoList';
+import { getCoronaInfo } from 'javascript/getCoronaInfo';
+import { CountryItem } from 'models';
+import { InfoList } from 'infoList';
 
-function Dashboard() {
-  const sampleData = {
-    NewConfirmed: 0,
-    TotalConfirmed: 0,
-    NewDeaths: 0,
-    TotalDeaths: 0,
-    NewRecovered: 0,
-    TotalRecovered: 0,
-    LastUpdate: new Date(),
+const Dashboard: React.FC = () => {
+  const [countryItems, setCountryItems] = useState<CountryItem[]>([]);
+
+  const getCountryItems = async () => {
+    const globalInfo = await getCoronaInfo();
+    setCountryItems(globalInfo);
   };
 
-  const [globalInfo, setGlobalInfo] = useState(sampleData);
-
   useEffect(() => {
-    (async () => {
-      const url = {
-        baseUrl: process.env.REACT_APP_REST_API_URL,
-        reqUrl: '/global-corona-info',
-      };
-      const globalInfo = await getCoronaInfo(url);
-      setGlobalInfo(globalInfo);
-    })();
+    getCountryItems();
   }, []);
 
   return (
     <>
-      <InfoList infos={globalInfo} />
+      <InfoList countryItems={countryItems} />
     </>
   );
-}
+};
 
 export default Dashboard;
--- src/infoList.tsx
+++ src/infoList.tsx
@@ -1,17 +1,118 @@
-import React from 'react';
+import { CountryItem } from 'models';
+import React, { FC, useMemo, useState } from 'react';
 import styled from 'styled-components';
 
-interface InfoListProps {
-  infos: {
-    NewConfirmed: number;
-    TotalConfirmed: number;
-    NewDeaths: number;
-    TotalDeaths: number;
-    NewRecovered: number;
-    TotalRecovered: number;
-    LastUpdate?: Date;
-  };
+interface Props {
+  countryItems: CountryItem[];
 }
+
+interface CountryInfoProps {
+  countryItem: CountryItem;
+  rank?: number;
+}
+
+export const InfoList: FC<Props> = ({ countryItems }) => {
+  return (
+    <>
+      <GlobalDiv>
+        <GlobalInfo countryItems={countryItems} />
+      </GlobalDiv>
+      <CountryMainDiv>
+        <CountryInfoList countryItems={countryItems} />
+      </CountryMainDiv>
+    </>
+  );
+};
+
+const GlobalInfo: FC<Props> = ({ countryItems }) => {
+  const globalInfo = useMemo(
+    () =>
+      countryItems.reduce(
+        (prev, curr) => ({
+          NewConfirmed: prev.NewConfirmed + curr.NewConfirmed,
+          TotalConfirmed: prev.TotalConfirmed + curr.TotalConfirmed,
+          NewDeaths: prev.NewDeaths + curr.NewDeaths,
+          TotalDeaths: prev.TotalDeaths + curr.TotalDeaths,
+          NewRecovered: prev.NewRecovered + curr.NewRecovered,
+          TotalRecovered: prev.TotalRecovered + curr.TotalRecovered,
+          LastUpdate: curr.LastUpdate,
+        }),
+        {
+          NewConfirmed: 0,
+          TotalConfirmed: 0,
+          NewDeaths: 0,
+          TotalDeaths: 0,
+          NewRecovered: 0,
+          TotalRecovered: 0,
+          LastUpdate: new Date(),
+        },
+      ),
+    [countryItems],
+  );
+
+  return (
+    <>
+      <GlobalInfoDiv>
+        <p>총 확진자 (단위: 명)</p>
+        <p>{globalInfo.TotalConfirmed}</p>
+        <p>+ ({globalInfo.NewConfirmed})</p>
+      </GlobalInfoDiv>
+      <GlobalInfoDiv>
+        <p>총 사망자 (단위: 명)</p>
+        <p>{globalInfo.TotalDeaths}</p>
+        <p>+ ({globalInfo.NewDeaths})</p>
+      </GlobalInfoDiv>
+      <GlobalInfoDiv>
+        <p>총 회복 (단위: 명)</p>
+        <p>{globalInfo.TotalRecovered}</p>
+        <p>+ ({globalInfo.NewRecovered})</p>
+      </GlobalInfoDiv>
+    </>
+  );
+};
+
+const CountryInfoList: FC<Props> = ({ countryItems }) => {
+  const sortedNew = useMemo(
+    () => countryItems.slice().sort((a, b) => b.NewConfirmed - a.NewConfirmed),
+    [countryItems],
+  );
+  const sortedTotal = useMemo(
+    () =>
+      countryItems.slice().sort((a, b) => b.TotalConfirmed - a.TotalConfirmed),
+    [countryItems],
+  );
+  return (
+    <>
+      <h2>신규 확진자 TOP 5</h2>
+      <CountryDiv>
+        {sortedNew.slice(0, 5).map((val, idx) => (
+          <CountryInfo countryItem={val} rank={idx + 1} key={idx} />
+        ))}
+      </CountryDiv>
+      <h2>총 확진자 TOP 5</h2>
+      <CountryDiv>
+        {sortedTotal.slice(0, 5).map((val, idx) => (
+          <CountryInfo countryItem={val} rank={idx + 1} key={idx} />
+        ))}
+      </CountryDiv>
+    </>
+  );
+};
+
+const CountryInfo: FC<CountryInfoProps> = ({ countryItem, rank }) => {
+  return (
+    <div>
+      <RankingSpan>{!rank || `#${rank}`}</RankingSpan>
+      <CountryInfoDiv>
+        <p>{countryItem.Country}</p>
+        <p>확진자: {countryItem.TotalConfirmed}</p>
+        <p>+ ({countryItem.NewConfirmed})</p>
+        <p>사망자: {countryItem.TotalDeaths}</p>
+        <p>+ ({countryItem.NewDeaths})</p>
+      </CountryInfoDiv>
+    </div>
+  );
+};
 
 const GlobalDiv = styled.div`
   height: 22vh;
@@ -21,7 +122,6 @@ const GlobalDiv = styled.div`
   justify-content: space-around;
   align-items: center;
 `;
-
 const GlobalInfoDiv = styled.div`
   background-color: #505050;
   flex: auto;
@@ -29,37 +129,34 @@ const GlobalInfoDiv = styled.div`
   text-align: center;
   padding: 16px;
   border-radius: 16px;
-  /*box-sizing: border-box;*/
 `;
-
-function GlobalInfo({ infos }: InfoListProps) {
-  return (
-    <>
-      <GlobalInfoDiv>
-        <p>총 확진자 (단위: 명)</p>
-        <p>{infos.TotalConfirmed}</p>
-        <p>+ ({infos.NewConfirmed})</p>
-      </GlobalInfoDiv>
-      <GlobalInfoDiv>
-        <p>총 사망자 (단위: 명)</p>
-        <p>{infos.TotalDeaths}</p>
-        <p>+ ({infos.NewDeaths})</p>
-      </GlobalInfoDiv>
-      <GlobalInfoDiv>
-        <p>총 회복 (단위: 명)</p>
-        <p>{infos.TotalRecovered}</p>
-        <p>+ ({infos.NewRecovered})</p>
-      </GlobalInfoDiv>
-    </>
-  );
-}
-
-function InfoList({ infos }: InfoListProps) {
-  return (
-    <GlobalDiv>
-      <GlobalInfo infos={infos} />
-    </GlobalDiv>
-  );
-}
-
-export default InfoList;
+const CountryMainDiv = styled.div`
+  width: 80%;
+  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  border-radius: 20px;
+  border: 3px solid black;
+`;
+const CountryDiv = styled.div`
+  padding: 16px;
+  color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  align-content: flex-start;
+  flex-wrap: wrap;
+`;
+const CountryInfoDiv = styled.div`
+  background-color: #505050;
+  flex: auto;
+  max-width: 150px;
+  text-align: center;
+  padding: 16px;
+  border-radius: 16px;
+`;
+const RankingSpan = styled.span`
+  color: black;
+  font-size: 20px;
+`;
