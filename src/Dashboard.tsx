fc99d59f1dc9082e8d4cf63932a661c629e457ad
a101201031 <0825tngus@naver.com>
2021-01-14 23:47:34 +0900

    feat: add Background image

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { InfoList } from 'InfoList';
 import { CountryItem } from 'models';
 import React from 'react';
+import styled from 'styled-components';
+import Earth from 'earth.jpg';
 
 interface Props {
   countryItems: CountryItem[];
@@ -8,8 +10,15 @@ interface Props {
 
 export const Dashboard: React.FC<Props> = ({ countryItems }) => {
   return (
-    <>
+    <DashboardContainer>
       <InfoList countryItems={countryItems} />
-    </>
+    </DashboardContainer>
   );
 };
+
+const DashboardContainer = styled.div`
+  background-image: url(${Earth});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+`;
