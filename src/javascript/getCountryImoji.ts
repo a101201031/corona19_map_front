13debf4b4d2efad7bf4a63429c5b78f6055c8366
a101201031 <0825tngus@naver.com>
2021-01-17 22:21:31 +0900

    feat: code remake (for -> array.map)

--- src/javascript/getCountryImoji.ts
+++ src/javascript/getCountryImoji.ts
@@ -1,8 +1,7 @@
 export const getCountryImoji = (cc: string): string => {
   const offset = 127397;
-  const codeArray: number[] = [];
-  for (let c of cc) {
-    codeArray.push(c.codePointAt(0)! + offset);
-  }
+  const codeArray: number[] = Array.from(cc).map(
+    (c) => c.codePointAt(0)! + offset,
+  );
   return String.fromCodePoint(...codeArray);
 };
